864944df8989fb846a7a5e64f0c47ba8a2130ee5
WillZealot <127908016+WillZealot@users.noreply.github.com>
2023-06-10 21:03:45 -0400

    added some java function that will make projects appear and the resume appear on that button click

--- develop/functions.js
+++ develop/functions.js
@@ -1,2 +1,30 @@
-document.addEventListener("DOMContentLoaded",)
+document.addEventListener('DOMContentLoaded', () => {
+    const worksBtnEl = document.querySelector(".worksArea");
+    const resumeBtnEl = document.querySelector(".resumeArea");
+    const projectsArea = document.querySelector(".bigProjectCard");
+    const resumeDisplay = document.querySelector(".resumePdf");
+
+    projectsArea.style.display = "none";
+    resumeDisplay.style.display = "none";
+
+    worksBtnEl.addEventListener("click", () => {
+        if (projectsArea.style.display === "none"){
+            projectsArea.style.display = "";
+        } else {
+            projectsArea.style.display = "none";
+        }
+    }
+        
+    );
+
+    resumeBtnEl.addEventListener("click", () => {
+        if (resumeDisplay.style.display === "none"){
+            resumeDisplay.style.display = "";
+        } else {
+            resumeDisplay.style.display = "none";
+        }
+    }
+        
+    );
     
+  });

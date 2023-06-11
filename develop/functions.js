9bb4e9b40def374f8f38c6de55f9655e9b137cf9
WillZealot <127908016+WillZealot@users.noreply.github.com>
2023-06-10 21:45:11 -0400

    smoothly appearing content area for projects and resume appear on button click so cool

--- develop/functions.js
+++ develop/functions.js
@@ -3,28 +3,44 @@ document.addEventListener('DOMContentLoaded', () => {
     const resumeBtnEl = document.querySelector(".resumeArea");
     const projectsArea = document.querySelector(".bigProjectCard");
     const resumeDisplay = document.querySelector(".resumePdf");
-
+  
+    // Adding transition styles
+    projectsArea.style.transition = "opacity 0.9s ease";
+    resumeDisplay.style.transition = "opacity 0.9s ease";
+  
+    // Hiding elements initially
+    projectsArea.style.opacity = "0";
+    resumeDisplay.style.opacity = "0";
     projectsArea.style.display = "none";
     resumeDisplay.style.display = "none";
-
+  
+    // Event listener for works button
     worksBtnEl.addEventListener("click", () => {
-        if (projectsArea.style.display === "none"){
-            projectsArea.style.display = "";
-        } else {
-            projectsArea.style.display = "none";
-        }
-    }
-        
-    );
-
+      if (projectsArea.style.display === "none") {
+        projectsArea.style.display = "";
+        setTimeout(() => {
+          projectsArea.style.opacity = "1";
+        }, 10);
+      } else {
+        projectsArea.style.opacity = "0";
+        setTimeout(() => {
+          projectsArea.style.display = "none";
+        }, 300);
+      }
+    });
+  
+    // Event listener for resume button
     resumeBtnEl.addEventListener("click", () => {
-        if (resumeDisplay.style.display === "none"){
-            resumeDisplay.style.display = "";
-        } else {
-            resumeDisplay.style.display = "none";
-        }
-    }
-        
-    );
-    
+      if (resumeDisplay.style.display === "none") {
+        resumeDisplay.style.display = "";
+        setTimeout(() => {
+          resumeDisplay.style.opacity = "1";
+        }, 10);
+      } else {
+        resumeDisplay.style.opacity = "0";
+        setTimeout(() => {
+          resumeDisplay.style.display = "none";
+        }, 300);
+      }
+    });
   });
